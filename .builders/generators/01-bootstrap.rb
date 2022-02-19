e04c5defe10139bb044622cf327f5bd0d0c192a6
David Cruwys <david@ideasmen.com.au>
2022-02-19 22:54:36 +1100

    first commit

---
+++ .builders/generators/01-bootstrap.rb
@@ -0,0 +1,86 @@
+KManager.action :bootstrap do
+  action do
+    application_name = :calculator_pure_css_html
+
+    director = KDirector::Dsls::BasicDsl
+      .init(k_builder,
+        on_exist:                   :skip,                      # %i[skip write compare]
+        on_action:                  :queue                      # %i[queue execute]
+      )
+      .data(
+        application:                application_name,
+        application_description:    'NoJS - Creating a Calculator with only pure HTML and CSS.',
+        author:                     'David Cruwys',
+        author_email:               'david@ideasmen.com.au',
+        initial_semver:             '1.0.0',
+        copyright_date:             '2022'
+      )
+      .github(
+        active: false,
+        repo_name: application_name,
+        organization: 'klueless-tut-html'
+      ) do
+        create_repository
+        # delete_repository
+        # list_repositories
+        open_repository
+        # run_command('git init')
+      end
+      .blueprint(
+        active: true,
+        name: :bin_hook,
+        description: 'initialize repository',
+        on_exist: :write) do
+
+        cd(:app)
+
+        run_command('git init')
+        add('.gitignore')
+
+        run_template_script('.git-setup.sh', dom: dom)
+
+        run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
+        run_command("gh repo edit -d \"#{dom[:application_description]}\"")
+      end
+      .package_json(
+        active: false,
+        name: :package_json,
+        description: 'Set up the package.json file for semantic versioning'
+      ) do
+        self
+          .add('package.json', dom: dom)
+          .play_actions
+
+        # self
+        #   .add_script('xxx', 'xxx')
+        #   .sort
+        #   .development
+        #   .npm_add_group('xxx')
+
+        # run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
+      end
+      .blueprint(
+        active: false,
+        name: :opinionated,
+        description: 'opinionated files',
+        on_exist: :write) do
+
+        cd(:app)
+
+        # add('something.txt', dom: dom)
+
+        # run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
+      end
+
+    director.play_actions
+  end
+end
+
+KManager.opts.app_name                    = 'calculator-pure-css-html'
+KManager.opts.sleep                       = 2
+KManager.opts.reboot_on_kill              = 0
+KManager.opts.reboot_sleep                = 4
+KManager.opts.exception_style             = :short
+KManager.opts.show.time_taken             = true
+KManager.opts.show.finished               = true
+KManager.opts.show.finished_message       = 'FINISHED :)'
